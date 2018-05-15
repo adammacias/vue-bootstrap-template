8b126b16b0a4f229ca265ca179a8167c3d194535
Adam Macias <eu@adammacias.com>
2018-05-15 18:44:55 -0300

    Update App.vue, add layout and modal component of BootstrapVue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <div class="container p-5">
+    <b-container class="p-5">
 
       <header>
         <h1>Bootstrap + Vue</h1>
@@ -10,36 +10,64 @@
       <!-- Colors -->
       <section class="mb-4">
         <h2 class="mb-3">Cores</h2>
-        <div class="row">
-          <div class="col-md-3 mb-2" v-for="color in themeColors">
+        <b-row>
+          <b-col class="mb-2" md="3" v-for="color in themeColors">
             <code>.{{ color }}</code>
             <div :class="'p-3 bg-blue bg-' + color"></div>
-          </div>
-        </div>
+          </b-col>
+        </b-row>
       </section>
 
       <!-- Buttons -->
       <section class="mb-4">
         <h2 class="mb-3">Botões</h2>
-        <div class="row">
-          <div class="col-md-3 mb-2" v-for="color in themeColors">
+        <b-row>
+          <b-col class="mb-2" md="3" v-for="color in themeColors">
             <button type="button" :class="'btn btn-block btn-' + color">{{ color }}</button>
             <button type="button" :class="'btn btn-block btn-outline-' + color">{{ color }}</button>
-          </div>
-        </div>
+          </b-col>
+        </b-row>
       </section>
 
-    </div>
+      <!-- Modal -->
+      <section class="mb-4">
+        <h2 class="mb-3">Modal</h2>
+
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-block btn-primary" @click="modalShow = !modalShow">
+          Abrir modal de demostração
+        </button>
+
+        <!-- Modal component -->
+        <b-modal
+          cancel-title="Cancelar"
+          centered
+          title="Título do Modal"
+          size="sm"
+          v-model="modalShow"
+          ok-title="Da hora!"
+          class="text-dark w-50 float-left">
+            Olá mundo! <a href="https://bootstrap-vue.js.org/docs/components/modal">Veja como me usar</a>.
+        </b-modal>
+      </section>
+
+    </b-container>
   </div>
 </template>
 
 <script>
+import bModal from 'bootstrap-vue/es/components/modal/modal'
+
 export default {
   name: 'app',
   data () {
     return {
-      themeColors: ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark']
+      themeColors: ['primary', 'secondary', 'success', 'danger', 'warning', 'info', 'light', 'dark'],
+      modalShow: false
     }
+  },
+  components: {
+    'b-modal': bModal
   }
 }
 </script>
